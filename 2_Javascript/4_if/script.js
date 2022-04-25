3463bbbf4d34f1d28d419fec2c6884581a7bba58
JINBOK LEE <eyelash1024@naver.com>
2022-04-25 17:45:03 +0900

    study and test

--- 2_Javascript/4_if/script.js
+++ 2_Javascript/4_if/script.js
@@ -121,3 +121,124 @@ if (age_i > 20) {
 } else {
   console.log("운전이 불가능합니다")
 }
+
+
+// test
+
+// q1.유저가 입력하는 숫자가 0인지 음성인지 양성인지 판단하는 프로그램을 만드시오
+
+let number = 10
+
+if (number < 0) {
+  console.log("음수 입니다")
+} else if (number > 0) {
+  console.log("양수 입니다")
+} else {
+  console.log("0 입니다")
+}
+
+// if와 else는 한 번만 사용할 수 있지만, else if는 여러 번 사용할 수 있다. 처음 오답은 if문 다음 별개의 if문을 또 달았어서 오류였다.
+
+
+
+// q2. 나는 대학교 교수다. 레포트 점수에따라 등급을 매기는 프로그램을 만드시오
+// 90~100 : A
+// 80~89 : B
+// 70~79 : C
+// 60~69 : D
+// less than 59 : F
+
+let score = 100
+
+if (90 <= score && score <= 100) {
+  console.log("A")
+} else if (80 <= score && score <= 89) {
+  console.log("B")
+} else if (70 <= score && score <= 79) {
+  console.log("C")
+} else if (60 <= score && score <= 69) {
+  console.log("D")
+} else if (0 <= score && score <= 59) {
+  console.log("F")
+} else {
+  console.log("잘못된 범위의 점수입니다")
+}
+
+// 주의! 비교할거면 사이에 and (&&) 연산자를 꼭 넣어야 한다! << 오답 냈었음
+
+
+
+// q3. 한 지원자가 우리회사에 지원을했다. 지원자가 사용가능한 스킬은 배열에 제공이 된다
+// let skills = ["HTML","CSS","Javascript","React"]
+// Javascript와 React 둘다 할줄 안다면 “합격!” Javascript와 React 둘중 하나만 할줄 안다면 “예비”, 두 스킬이 없다면 “탈락” 을 보여주는 프로그램을 짜시오
+
+// input : let skills = ["HTML","CSS","Javascript","React"]
+// output: 합격 
+
+// input:let skills = ["HTML","CSS","Javascript"] 
+// output: 예비 
+
+// input:let skills = ["HTML","CSS"] 
+// output: 탈락
+
+
+// 작성한 오답
+// let skills1 = ["HTML", "CSS", "Javascript", "React"]
+
+//if (skills1.includes("Javascript" && "React")) {
+//  console.log("합격")
+//} else if (skills1.includes("Javascript" || "React")) {
+//  console.log("예비")
+//} else {
+//  console.log("탈락")
+//}
+
+//let skills2 = ["HTML", "CSS", "React"]
+
+//if (skills2.includes("Javascript" && "React")) {
+//  console.log("합격")
+//} else if (skills2.includes("Javascript" || "React")) {
+//  console.log("예비")
+//} else {
+//  console.log("탈락")
+//}
+
+//let skills3 = ["HTML", "CSS"]
+
+//if (skills3.includes("Javascript" && "React")) {
+//  console.log("합격")
+//} else if (skills3.includes("Javascript" || "React")) {
+//  console.log("예비")
+//} else {
+//  console.log("탈락")
+//}
+
+let skills1 = ["HTML", "CSS", "Javascript", "React"]
+
+if (skills1.includes("Javascript") && skills1.includes("React")) {
+  console.log("합격")
+} else if (skills1.includes("Javascript") || skills1.includes("React")) {
+  console.log("예비")
+} else {
+  console.log("탈락")
+}
+
+let skills2 = ["HTML", "CSS", "React"]
+
+if (skills2.includes("Javascript") && skills2.includes("React")) {
+  console.log("합격")
+} else if (skills2.includes("Javascript") || skills2.includes("React")) {
+  console.log("예비")
+} else {
+  console.log("탈락")
+}
+
+let skills3 = ["HTML", "CSS"]
+
+if (skills3.includes("Javascript") && skills3.includes("React")) {
+  console.log("합격")
+} else if (skills3.includes("Javascript") || skills3.includes("React")) {
+  console.log("예비")
+} else {
+  console.log("탈락")
+}
