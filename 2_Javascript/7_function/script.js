219b5ae38336fd47c87ca61e73b686ae0e6d34c6
JINBOK LEE <eyelash1024@naver.com>
2022-04-26 16:10:06 +0900

    study -ing

---
+++ 2_Javascript/7_function/script.js
@@ -0,0 +1,31 @@
+console.log("빵두기")
+console.log("상추두기")
+console.log("고기패티두기")
+console.log("뚜껑덮기")
+
+console.log("감튀박스선택")
+console.log("감튀담기")
+
+console.log("콜라통선택")
+console.log("얼음담기")
+console.log("콜라담기")
+
+function makeBurger() {
+  console.log("빵두기")
+  console.log("상추두기")
+  console.log("고기패티두기")
+  console.log("뚜껑덮기")
+}
+
+function makeFrenchfries() {
+  console.log("감튀박스선택")
+  console.log("감튀담기")
+}
+
+function serveCoke() {
+  console.log("콜라통선택")
+  console.log("얼음담기")
+  console.log("콜라담기")
+}
+
+serveCoke() // 이처럼 함수이름을 입력해주어야 작동한다. 그전까진 작동 X
