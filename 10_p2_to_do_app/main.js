2afbc1241e088bd62aa75b176331ffe14958ec03
JINBOK LEE <eyelash1024@naver.com>
2022-05-10 23:09:03 +0900

    CHECK Button toggle function implemente

--- 10_p2_to_do_app/main.js
+++ 10_p2_to_do_app/main.js
@@ -4,27 +4,62 @@ addButton.addEventListener("click", addTask);
 let taskList = [];
 let taskBar = document.getElementById("task_list");
 
-
 function addTask() {
-  taskList.push(taskInput.value);
+  let task = {
+    id: randomIDGenerator(),
+    taskName: taskInput.value,
+    isComplete: false
+  };
+  taskList.push(task);
   console.log(taskList);
   render()
 }
 
 function render() {
+
   let resultHTML = ''
 
   for (i = 0; i < taskList.length; i++) {
-    resultHTML += `
+
+    if (taskList[i].isComplete == true) {
+      resultHTML +=
+        `
     <div class="task_list_area">
-    <span>${taskList[i]}</span>
+    <span class="task_done">${taskList[i].taskName}</span>
     <span>
-      <button>CHECK</button>
+      <button onclick="toggleComplete('${taskList[i].id}')">CHECK</button>
       <button>DELETE</button>
     </span>
   </div>
   `
+    } else {
+      resultHTML +=
+        `
+    <div class="task_list_area">
+    <span>${taskList[i].taskName}</span>
+    <span>
+      <button onclick="toggleComplete('${taskList[i].id}')">CHECK</button>
+      <button>DELETE</button>
+    </span>
+  </div>
+  `
+    }
   }
 
   document.getElementById("task_list").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  console.log(taskList);
+  render()
+}
+
+function randomIDGenerator() {
+  return (performance.now().toString(36) + Math.random().toString(36)).replace(/\./g, "");
 }
