bfbb9a42752a930e20baad065abce25e0d16823f
JINBOK LEE <eyelash1024@naver.com>
2022-05-16 13:21:01 +0900

    solve some question

--- 11_ES6_practice/practice2.js
+++ 11_ES6_practice/practice2.js
@@ -29,16 +29,18 @@ let names = [
 // )
 // console.log(initialOnly)
 
+
+
 // filter 문제
 // 이름에 a를 포함한 사람들을 출력하시오.
 // 이름에 같은 글자가 연속해서 들어간 사람을 출력하시오.(예 - tt, ff, ll 이런 글자들)
 
-let aPerson = names.filter(
-  (item) => {
-    return item.includes('a')
-  }
-)
-console.log(aPerson)
+// let aPerson = names.filter(
+//   (item) => {
+//     return item.includes('a')
+//   }
+// )
+// console.log(aPerson)
 
 // let consecutiveLettersPerson = names.filter( // 오답
 //   (item) => {
@@ -48,18 +50,36 @@ console.log(aPerson)
 //   }
 // )
 
-let doubleLetter =  names.filter((item) => {
-  let splitName = item.split(""); 
-  // 1. 이름부터 분해한다
-  return splitName.some((letter, index) => letter == splitName[index + 1]);
-  // 2. 분해한 이름에서 letter 인자와 분해한 이름의 +1인덱스와 같은지 비교하고, some 함수를 이용해 true인 값을 모두 return 시킨다
-})
+// let doubleLetter =  names.filter((item) => {
+//   let splitName = item.split(""); 
+//   // 1. 이름부터 분해한다
+//   return splitName.some((letter, index) => letter == splitName[index + 1]);
+//   // 2. 분해한 이름에서 letter 인자와 분해한 이름의 +1인덱스와 같은지 비교하고, some 함수를 이용해 true인 값을 모두 return 시킨다
+// })
 
-console.log(doubleLetter)
+// console.log(doubleLetter)
+
+
 
 // some 문제
 // 전체 이름의 길이가 20자 이상인 사람이 있는가?
 // 성을 제외한 이름에 p를 포함한 사람이 있는가?(대소문자 상관 no)
+
+// let over20Letter = names.some((item) => {return item.length > 20})
+// console.log(over20Letter)
+// // 또는,
+// console.log(names.some((item)=>item.length>=20))
+
+// let justName = names.map((item)=>{return item.split(' ')[0]})
+// let pInclude = justName.some((item)=>{return item.includes("p")})
+// console.log(pInclude)
+
+// 아래는 모범답안
+// console.log(names.some((item)=>{
+//   let splitName = item.split(' ')
+//   splitName.pop()
+//   return splitName.some(eachName=>eachName.toLocaleLowerCase().includes("p"))
+// }))
 
 // every 문제
 // 모두의 전체 이름의 길이가 20자 이상인가?
