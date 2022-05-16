5545f1afb9442fba5d24a4fa02334915845fe025
JINBOK LEE <eyelash1024@naver.com>
2022-05-16 12:49:10 +0900

    solve filter question

--- 11_ES6_practice/practice2.js
+++ 11_ES6_practice/practice2.js
@@ -31,7 +31,31 @@ let names = [
 
 // filter 문제
 // 이름에 a를 포함한 사람들을 출력하시오.
-// 이름에 같은 글자가 연속해서 들어간 사람을 출력하시오. (예-tt,ff,ll 이런 글자들)
+// 이름에 같은 글자가 연속해서 들어간 사람을 출력하시오.(예 - tt, ff, ll 이런 글자들)
+
+let aPerson = names.filter(
+  (item) => {
+    return item.includes('a')
+  }
+)
+console.log(aPerson)
+
+// let consecutiveLettersPerson = names.filter( // 오답
+//   (item) => {
+//     if (item.split('') == item.split('')+1) {
+//     return item.split('')
+//     }
+//   }
+// )
+
+let doubleLetter =  names.filter((item) => {
+  let splitName = item.split(""); 
+  // 1. 이름부터 분해한다
+  return splitName.some((letter, index) => letter == splitName[index + 1]);
+  // 2. 분해한 이름에서 letter 인자와 분해한 이름의 +1인덱스와 같은지 비교하고, some 함수를 이용해 true인 값을 모두 return 시킨다
+})
+
+console.log(doubleLetter)
 
 // some 문제
 // 전체 이름의 길이가 20자 이상인 사람이 있는가?
