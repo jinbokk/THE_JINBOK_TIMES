96db991a14b45c4169a152709960ca2b5f77784c
JINBOK LEE <eyelash1024@naver.com>
2022-05-18 19:18:53 +0900

    get news by topic function added

--- 12_p3_news_API/main.js
+++ 12_p3_news_API/main.js
@@ -1,7 +1,14 @@
 let newsArticles = [];
+let menus = document.querySelectorAll(".main_buttons button")
+
+menus.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    getNewsByTopic(event)
+  })
+})
 
 const getLatestNews = async () => {
-  let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10');
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
 
   let header = new Headers({
     'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
@@ -22,6 +29,26 @@ const getLatestNews = async () => {
 
 getLatestNews();
 
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase()
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`)
+  let header = new Headers({
+    'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
+  });
+
+  let response = await fetch(url, {
+    headers: header
+  });
+
+  let data = await response.json();
+
+  newsArticles = data.articles;
+
+  console.log(newsArticles);
+
+  render();
+}
+
 const render = () => {
   let newsArticlesHTML = "";
 
@@ -29,7 +56,7 @@ const render = () => {
     return `<div class="row py-4">
  <div class="col-lg-4">
    <img class="news_img"
-     src=${item.media == null || item.media == "" ? "images\no-image.png" : item.media} />
+     src="${item.media || "./images/no-image.png"}"/>
  </div>
  <div class="col-lg-8 position-relative">
    <h2 class="pt-3 fw-bold">${item.title}</h2>
@@ -41,7 +68,7 @@ const render = () => {
      : item.summary.length > 200
      ? item.summary.substring(0,200) + "..."
      : item.summary}</p>
-   <div>${item.clean_url == null || item.clean_url == "" ? "no source" : item.clean_url } // ${moment(item.published_date).fromNow()}</div>
+   <div>${item.clean_url || "no source"} // ${moment(item.published_date).fromNow()}</div>
  </div>
 </div>`;
   }).join('');
