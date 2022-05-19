2b3b592bf4010fdc2d8ac037b487e4cd88cc68c7
JINBOK LEE <eyelash1024@naver.com>
2022-05-19 15:09:20 +0900

    keyword search function added

--- 12_p3_news_API/main.js
+++ 12_p3_news_API/main.js
@@ -1,5 +1,5 @@
 let newsArticles = [];
-let menus = document.querySelectorAll(".main_buttons button")
+let menus = document.querySelectorAll(".main_buttons button");
 
 menus.forEach((item) => {
   item.addEventListener("click", (event) => {
@@ -9,54 +9,25 @@ menus.forEach((item) => {
 
 const getLatestNews = async () => {
   let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
-
   let header = new Headers({
     'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
   });
-
   let response = await fetch(url, {
     headers: header
   }); // ajax, http, fetch 등을 이용할 수도 있다. 강의에서는 fetch 추천.
-
   let data = await response.json(); // data 또한 await을 해주어야 한다. 상위에서 await 해서 받아오는 response의 data를 가져오는 것이기 때문이다.
-
   newsArticles = data.articles;
-
   console.log(newsArticles);
-
   render();
 };
 
-getLatestNews();
-
-const getNewsByTopic = async (event) => {
-  let topic = event.target.textContent.toLowerCase()
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`)
-  let header = new Headers({
-    'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
-  });
-
-  let response = await fetch(url, {
-    headers: header
-  });
-
-  let data = await response.json();
-
-  newsArticles = data.articles;
-
-  console.log(newsArticles);
-
-  render();
-}
-
 const render = () => {
   let newsArticlesHTML = "";
-
   newsArticlesHTML = newsArticles.map((item) => {
     return `<div class="row py-4">
  <div class="col-lg-4">
    <img class="news_img"
-     src="${item.media || "./images/no-image.png"}"/>
+     src="${item.media || "./images/no-image.png"}" onerror="this.src='./images/no-image.png';"/>
  </div>
  <div class="col-lg-8 position-relative">
    <h2 class="pt-3 fw-bold">${item.title}</h2>
@@ -72,6 +43,41 @@ const render = () => {
  </div>
 </div>`;
   }).join('');
-
   document.getElementById("news_articles").innerHTML = newsArticlesHTML;
 };
+
+getLatestNews();
+
+const getNewsByTopic = async (event) => {
+  let topic = event.target.textContent.toLowerCase()
+  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`)
+  let header = new Headers({
+    'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
+  });
+  let response = await fetch(url, {
+    headers: header
+  });
+  let data = await response.json();
+  newsArticles = data.articles;
+  console.log(newsArticles);
+  render();
+}
+
+let searchButton = document.getElementById("search_button");
+
+const getNewsByKeyword = async () => {
+  let keyword = document.getElementById("search_input").value;
+  let url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&countries=KR&page_size=10`);
+  let header = new Headers({
+    'x-api-key': 'UdwQsPDZDWoKpyITsqOqxQU9HzZqtsfadf3SA24oVpE'
+  });
+  let response = await fetch(url, {
+    headers: header
+  });
+  let data = await response.json();
+  newsArticles = data.articles;
+  console.log(newsArticles);
+  render();
+}
+
+searchButton.addEventListener("click", getNewsByKeyword)
