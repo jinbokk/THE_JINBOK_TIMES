62c22a500d297368f05d9739ebee3ffdb6294b7d
JINBOK LEE <eyelash1024@naver.com>
2022-05-21 19:50:38 +0900

    pagination added

--- 12_p3_news_API/main.js
+++ 12_p3_news_API/main.js
@@ -1,6 +1,8 @@
 let newsArticles = [];
 let menus = document.querySelectorAll(".main_buttons button");
 let url;
+let page = 1;
+let total_pages = 0;
 
 menus.forEach((item) => {
   item.addEventListener("click", (event) => {
@@ -19,13 +21,17 @@ const getNews = async () => {
     }); // ajax, http, fetch 등을 이용할 수도 있다. 강의에서는 fetch 추천.
 
     let data = await response.json(); // data 또한 await을 해주어야 한다. 상위에서 await 해서 받아오는 response의 data를 가져오는 것이기 때문이다.
-    console.log(data)
+
     if (response.status == 200) {
+      console.log(data)
+      newsArticles = data.articles;
+      // page = data.page;
+      // total_pages = data.total_pages;
+      render();
+
       if (data.status == "No matches for your search.") {
         throw new Error("No Matches For Your Search!")
       }
-      newsArticles = data.articles;
-      render();
     } else {
       throw new Error("Page Error!")
     }
@@ -41,31 +47,35 @@ const getNews = async () => {
 }
 
 const getLatestNews = async () => {
-  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
+  url = new URL(`
+      https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10`);
   getNews()
 };
 
 const render = () => {
   let newsArticlesHTML = "";
   newsArticlesHTML = newsArticles.map((item) => {
-    return `<div class="row py-4">
- <div class="col-lg-4">
-   <img class="news_img"
-     src="${item.media || "./images/no-image.png"}" onerror="this.src='./images/no-image.png';"/>
- </div>
- <div class="col-lg-8 position-relative">
-   <h2 class="pt-3 fw-bold">${item.title}</h2>
-   <p class="py-2">${
-     item.summary == null || item.summary == ""
-     ? "내용 없음"
-     : item.summary.length < 200
-     ? "본문을 보려면 클릭하십시오"
-     : item.summary.length > 200
-     ? item.summary.substring(0,200) + "..."
-     : item.summary}</p>
-   <div>${item.clean_url || "no source"} // ${moment(item.published_date).fromNow()}</div>
- </div>
-</div>`;
+    return `
+    <div class="row py-4">
+      <div class="col-lg-4">
+        <img class="news_img" src="${item.media || "./images/no-image.png"}" onerror="this.src='./images/no-image.png';"/>
+      </div> 
+        <div class = "col-lg-8 position-relative" >
+          <h2 class="pt-3 fw-bold">${item.title}</h2>
+            <p class="py-2">
+            ${item.summary == null || item.summary == ""
+              ? "내용 없음"
+              : item.summary.length < 200
+              ? "본문을 보려면 클릭하십시오"
+              : item.summary.length > 200
+              ? item.summary.substring(0,200) + "..."
+              : item.summary}
+            </p>
+              <div>
+                ${item.clean_url || "no source"} // ${moment(item.published_date).fromNow()}
+              </div>
+        </div>
+    </div>`;
   }).join('');
   document.getElementById("news_articles").innerHTML = newsArticlesHTML;
 };
@@ -93,6 +103,22 @@ searchInput.addEventListener("keypress", (event) => {
     getNewsByKeyword()
   }
 });
+
+const pagination = () => {
+  let paginationHTML = ``
+  let pageGroup = Math.ceil(page / 5);
+  let last = pageGroup * 5;
+  let first = last - 4;
+
+  console.log(pageGroup, last, first)
+
+  for (let i = first; i <= last; i++) {
+    paginationHTML += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`
+  }
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+}
+
+pagination()
 
 // let eachNews = document.querySelectorAll(".row")
 // console.log(eachNews)
