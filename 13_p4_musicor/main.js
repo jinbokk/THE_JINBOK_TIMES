4a261f4ec19739f7d9eb3ddf55de4034cc944d20
JINBOK LEE <eyelash1024@naver.com>
2022-05-30 17:19:01 +0900

    Attempt to use Spotify API_2

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -1,63 +1,36 @@
-// var SpotifyWebApi = require('spotify-web-api-node');
+const APIController = (function () {
 
-// // credentials are optional
-// var spotifyApi = new SpotifyWebApi({
-//   clientId: 'f5d847d6012d455ab5e1c80a3011f215',
-//   clientSecret: 'c618878d472a4592a9c37b6d7f4fe839',
-//   redirectUri: 'http://www.example.com/callback'
-// });
+  const clientId = 'f5d847d6012d455ab5e1c80a3011f215';
+  const clientSecret = 'a5fbc11da2cf4878a5bdd9998a14c64a';
 
+  const _getToken = async () => {
 
-// spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-//   function(data) {
-//     console.log('Artist albums', data.body);
-//   },
-//   function(err) {
-//     console.error(err);
-//   }
-// );
-
-
-/**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
- */
-
-var request = require('request'); // "Request" library
-
-var client_id = 'f5d847d6012d455ab5e1c80a3011f215'; // Your client id
-var client_secret = 'c618878d472a4592a9c37b6d7f4fe839'; // Your secret
-
-// your application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
-
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://musicor.netlify.app',
+    const result = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
       headers: {
-        'Authorization': 'Bearer ' + token
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic' + btoa(clientId + ':' + clientSecret)
       },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
+      body: 'grant+type=client_credentials'
     });
+
+    const data = await result.json();
+    return data.access_token;
   }
-});
+
+})();
+
+const _getGenres = async (token) => {
+
+  const result = await fetch('https://api.spotify.com/v1/brows/categories?locale=sv_US', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer' + token
+    }
+  });
+
+  const data = await result.json();
+  return data.categories.items;
+}
+
+console.log(_getGenres())
