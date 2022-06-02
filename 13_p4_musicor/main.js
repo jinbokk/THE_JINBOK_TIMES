30c430c06b994264ea20c80e2414bb051e01cedc
JINBOK LEE <eyelash1024@naver.com>
2022-06-02 20:30:50 +0900

    add random bgImg

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -6,19 +6,41 @@
 //https://www.contentful.com/developers/docs/references/images-api/#/reference/changing-formats/progressive-jpegs/retrieve-an-image/console/js
 
 
-const getToken = async () => {
-  let url = new URL('http://www.last.fm/api/auth/');
+// const getToken = async () => {
+//   let url = new URL('http://www.last.fm/api/auth/');
 
-  let headers = new Headers({
-    'api_key': 'c86ba179640ade3c2fa8346ff63f5dc6'
-  });
+//   let headers = new Headers({
+//     'api_key': 'c86ba179640ade3c2fa8346ff63f5dc6'
+//   });
 
-  let response = await fetch(url, {
-    headers: header
-  });
+//   let response = await fetch(url, {
+//     headers: header
+//   });
 
-  let data = await response.json();
+//   let data = await response.json();
 
-  console.log(data);
+//   console.log(data);
+// }
+// getToken()
+
+
+const bgImgRender = () => {
+  let imgs = [
+    "/images/freepik/freepik_background_ (1).webp",
+    "/images/freepik/freepik_background_ (2).webp",
+    "/images/freepik/freepik_background_ (3).webp",
+    "/images/freepik/freepik_background_ (4).webp"
+  ];
+
+  let bg_img = imgs[Math.floor(Math.random() * imgs.length)]
+
+  let bg = document.getElementById("background");
+
+  bg.style.backgroundImage = 'url("' + bg_img + '")';
+  bg.style.backgroundRepeat = "no-repeat";
+  bg.style.backgroundSize = "cover";
+
+  console.log(bg_img)
 }
-getToken()
+
+bgImgRender()
