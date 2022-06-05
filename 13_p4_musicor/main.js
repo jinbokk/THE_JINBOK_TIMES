cf47f99fa0f50f443998ca2b1f03f953bb2e9298
JINBOK LEE <eyelash1024@naver.com>
2022-06-05 19:51:17 +0900

    rewrite the layout and code

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -1,44 +1,12 @@
-// API Key: c86ba179640ade3c2fa8346ff63f5dc6
-
-// Shared Secret: -
-
-// image api
-//https://www.contentful.com/developers/docs/references/images-api/#/reference/changing-formats/progressive-jpegs/retrieve-an-image/console/js
-
-
-// const getToken = async () => {
-//   let url = new URL('http://www.last.fm/api/auth/');
-
-//   let headers = new Headers({
-//     'api_key': 'c86ba179640ade3c2fa8346ff63f5dc6'
-//   });
-
-//   let response = await fetch(url, {
-//     headers: header
-//   });
-
-//   let data = await response.json();
-
-//   console.log(data);
-// }
-// getToken()
-
-
-const bgImgRender = () => {
-  let imgs = [
-    "/images/freepik/freepik_background_ (1).webp",
-    // "/images/freepik/freepik_background_ (2).webp",
-    "/images/freepik/freepik_background_ (3).webp",
-    "/images/freepik/freepik_background_ (4).webp"
+const randomImgRender = () => {
+  let mainImgs = [
+    "/images/freepik/freepik_background_ (1).jpg",
+    "/images/freepik/freepik_background_ (2).jpg",
+    "/images/freepik/freepik_background_ (3).jpg",
   ];
+  let mainImgSelect = mainImgs[Math.floor(Math.random() * mainImgs.length)]
 
-  let bg_img = imgs[Math.floor(Math.random() * imgs.length)]
-
-  let bg = document.getElementById("background");
-
-  bg.style.backgroundImage = 'url("' + bg_img + '")';
-  bg.style.backgroundRepeat = "no-repeat";
-  bg.style.backgroundSize = "cover";
-  // bg.style.backgroundPosition = "50% 50%";
+  document.getElementById("main_section").innerHTML +=
+  '<img src="'+mainImgSelect+'"/>'
 }
-bgImgRender()
+randomImgRender()
