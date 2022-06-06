de728b250b4a477c1a7a7afde3a1126870a52b7d
JINBOK LEE <eyelash1024@naver.com>
2022-06-06 20:25:28 +0900

    fadeIn animation added

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -10,19 +10,3 @@ const randomImgRender = () => {
   '<img src="'+mainImgSelect+'"/>'
 }
 randomImgRender()
-
-// ----------------------------------------- swiper 
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  slidesPerView : 3,
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-});
