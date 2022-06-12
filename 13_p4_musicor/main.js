2a092735e29118f352f938fed70ec7bea3e95bcc
JINBOK LEE <eyelash1024@naver.com>
2022-06-12 16:11:19 +0900

    complete animation by scroll

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -7,7 +7,7 @@ const randomImgRender = () => {
   let mainImgSelect = mainImgs[Math.floor(Math.random() * mainImgs.length)]
 
   document.getElementById("subTitle_section").innerHTML +=
-    '<img src="' + mainImgSelect + '" class="fadeInLeft"/>'
+    '<img src="' + mainImgSelect + '" class="scrolled fade-in"/>'
 
   // let blackImgs = [
   //   "/images/Pexels/black/pexels_black.jpg",
@@ -86,3 +86,45 @@ const randomImgRender = () => {
   //   '<img src="' + greenSelect + '"/>'
 }
 randomImgRender()
+
+ 
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
