0bdfd43ba705a078a18c390bc7fb65bfce1857f7
JINBOK LEE <eyelash1024@naver.com>
2022-06-15 17:31:02 +0900

    Use browserify for use Node.js in WebBrowser

--- 13_p4_musicor/config.js
+++ 13_p4_musicor/config.js
@@ -1,8 +1,8 @@
-var request = require('request');
-var client_id = 'f5d847d6012d455ab5e1c80a3011f215';
-var client_secret = 'a5fbc11da2cf4878a5bdd9998a14c64a';
+let request = require('request');
+let client_id = 'f5d847d6012d455ab5e1c80a3011f215';
+let client_secret = 'a5fbc11da2cf4878a5bdd9998a14c64a';
 
-var authOptions = {
+let authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
@@ -13,19 +13,9 @@ var authOptions = {
   json: true
 };
 
-
 request.post(authOptions, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/recommendations/available-genre-seeds',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
+    let token = body.access_token;
+    console.log(token)
   }
 });
