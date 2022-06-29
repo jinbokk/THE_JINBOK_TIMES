ff23dcade085ed09d08e637e46ccd9c84b07cd56
JINBOK LEE <eyelash1024@naver.com>
2022-06-29 16:44:35 +0900

    prettier code

--- 13_p4_musicor/main.js
+++ 13_p4_musicor/main.js
@@ -4,10 +4,10 @@ const randomImgRender = (function () {
     "/images/freepik/freepik_background_ (2).jpg",
     "/images/freepik/freepik_background_ (3).jpg",
   ];
-  let mainImgSelect = mainImgs[Math.floor(Math.random() * mainImgs.length)]
+  let mainImgSelect = mainImgs[Math.floor(Math.random() * mainImgs.length)];
 
   document.getElementById("subTitle_section").innerHTML +=
-    '<img src="' + mainImgSelect + '" class="scrolled fade-in"/>'
+    '<img src="' + mainImgSelect + '" class="scrolled fade-in"/>';
 })();
 
 const scrollElements = document.querySelectorAll(".js-scroll");
@@ -33,29 +33,21 @@ const displayScrollElement = (element) => {
   element.classList.add("scrolled");
 };
 
-// const hideScrollElement = (element) => {
-//   element.classList.remove("scrolled");
-// };
-
 const handleScrollAnimation = () => {
   scrollElements.forEach((el) => {
     if (elementInView(el, 1.25)) {
       displayScrollElement(el);
-    } 
-    // else if (elementOutofView(el)) {
-    //   hideScrollElement(el)
-    // }
-  })
-}
+    }
+  });
+};
 
 window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
 
+const page_pointer = document.querySelectorAll(".pagePointer span");
 
-const page_pointer = document.querySelectorAll(".pagePointer span")
-
-const pages = document.querySelectorAll("section")
+const pages = document.querySelectorAll("section");
 const navOffsetTop = document.querySelector("nav").offsetHeight;
 
 const firstTop = pages[0].offsetTop - navOffsetTop;
@@ -65,20 +57,20 @@ const thirdTop = pages[2].offsetTop - navOffsetTop;
 page_pointer[0].addEventListener("click", () => {
   window.scroll({
     top: firstTop,
-    behavior: "smooth"
-  })
-})
+    behavior: "smooth",
+  });
+});
 
 page_pointer[1].addEventListener("click", () => {
   window.scroll({
     top: secondTop,
-    behavior: "smooth"
-  })
-})
+    behavior: "smooth",
+  });
+});
 
 page_pointer[2].addEventListener("click", () => {
   window.scroll({
     top: thirdTop,
-    behavior: "smooth"
-  })
-})
+    behavior: "smooth",
+  });
+});
--- 13_p4_musicor/spotifyAPI_JB.js
+++ 13_p4_musicor/spotifyAPI_JB.js
@@ -1,36 +1,36 @@
 // Get Access Token value
 const getToken = async () => {
-  const clientId = 'f5d847d6012d455ab5e1c80a3011f215';
-  const clientSecret = 'a5fbc11da2cf4878a5bdd9998a14c64a';
-  const result = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
+  const clientId = "f5d847d6012d455ab5e1c80a3011f215";
+  const clientSecret = "a5fbc11da2cf4878a5bdd9998a14c64a";
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
     },
-    body: 'grant_type=client_credentials'
+    body: "grant_type=client_credentials",
   });
   const data = await result.json();
   const token = data.access_token;
   return token;
-}
+};
 getToken();
-
 
 // Get Available Genre Seeds to give genre to color
 const getAvailableGenreSeeds = async () => {
-
   const token = await getToken();
-  const result = await fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + token
+  const result = await fetch(
+    `https://api.spotify.com/v1/recommendations/available-genre-seeds`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
     }
-  });
+  );
   const data = await result.json();
   return data;
 };
-
 
 // Give genre to color from genre seeds
 const giveGenreToColor = async () => {
@@ -38,68 +38,85 @@ const giveGenreToColor = async () => {
 
   const genreSet = {
     genres_black: [genreData.genres[52]],
-    genres_gray: [genreData.genres[52], genreData.genres[2], genreData.genres[94], genreData.genres[16]],
+    genres_gray: [
+      genreData.genres[52],
+      genreData.genres[2],
+      genreData.genres[94],
+      genreData.genres[16],
+    ],
     genres_violet: [genreData.genres[94]],
     genres_yellow: [genreData.genres[94], genreData.genres[100]],
     genres_white: [genreData.genres[4], genreData.genres[85]],
-    genres_orange: [genreData.genres[18], genreData.genres[31], genreData.genres[124]],
+    genres_orange: [
+      genreData.genres[18],
+      genreData.genres[31],
+      genreData.genres[124],
+    ],
     genres_red: [genreData.genres[8], genreData.genres[66]],
     genres_blue: [genreData.genres[94]],
-    genres_green: [genreData.genres[0], genreData.genres[102], genreData.genres[85]]
-  }
+    genres_green: [
+      genreData.genres[0],
+      genreData.genres[102],
+      genreData.genres[85],
+    ],
+  };
 
   const searchIndexByColor = {
-    black: 'target_energy=0.9&target_danceability=0.1&min_tempo=50',
-    gray: 'target_energy=0.1&target_danceability=0.1&min_tempo=40',
-    violet: 'target_energy=0.5&target_danceability=0.5&min_tempo=60',
-    yellow: 'target_energy=0.9&target_danceability=0.9&min_tempo=144',
-    white: 'min_instrumentalness=0.9&target_energy=0&target_danceability=0',
-    orange: 'target_energy=1&target_danceability=1&min_tempo=80',
-    red: 'target_energy=0.2&target_danceability=0.2&max_tempo=80',
-    blue: 'target_energy=0.7&target_danceability=0.7&min_tempo=60',
-    green: 'target_energy=0&target_danceability=0&man_tempo=70'
-  }
+    black: "target_energy=0.9&target_danceability=0.1&min_tempo=50",
+    gray: "target_energy=0.1&target_danceability=0.1&min_tempo=40",
+    violet: "target_energy=0.5&target_danceability=0.5&min_tempo=60",
+    yellow: "target_energy=0.9&target_danceability=0.9&min_tempo=144",
+    white: "min_instrumentalness=0.9&target_energy=0&target_danceability=0",
+    orange: "target_energy=1&target_danceability=1&min_tempo=80",
+    red: "target_energy=0.2&target_danceability=0.2&max_tempo=80",
+    blue: "target_energy=0.7&target_danceability=0.7&min_tempo=60",
+    green: "target_energy=0&target_danceability=0&man_tempo=70",
+  };
 
   const colorContainer = document.querySelector(".colorPickContainer");
 
   colorContainer.addEventListener("click", (e) => {
+    const color = e.target;
+    const colorId = color.id;
 
-    const color = e.target
-    const colorId = color.id
+    if (color.classList.contains("colorContainer")) {
+      const colorPickSection = document.getElementById("color_container");
 
-    if (color.classList.contains('colorContainer')) {
-
-
-      const colorPickSection = document.getElementById("color_container")
-
-      gsap.to(colorPickSection, {
-        duration: 1,
-        scale: .9,
-      }).then(gsap.to(colorPickSection, {
-        duration: 2,
-        opacity: 0,
-        ease: "power4.out",
-        x: "-100%",
-        delay: .3
-      }))
+      gsap
+        .to(colorPickSection, {
+          duration: 1,
+          scale: 0.9,
+        })
+        .then(
+          gsap.to(colorPickSection, {
+            duration: 2,
+            opacity: 0,
+            ease: "power4.out",
+            x: "-100%",
+            delay: 0.3,
+          })
+        );
 
       const searchIndex = eval(`searchIndexByColor.${colorId}`);
       const genresByColor = eval(`genreSet.genres_${colorId}`);
-      const num = (Math.floor(Math.random() * genresByColor.length));
+      const num = Math.floor(Math.random() * genresByColor.length);
       const genreIdByColor = genresByColor[num];
 
       const getTrackByGenre = async () => {
         const token = await getToken();
         const genreId = genreIdByColor;
-        const result = await fetch(`https://api.spotify.com/v1/recommendations/?seed_genres=${genreId}&market=US&${searchIndex}&min-popularity=80?limit=30`, {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Bearer ' + token
+        const result = await fetch(
+          `https://api.spotify.com/v1/recommendations/?seed_genres=${genreId}&market=US&${searchIndex}&min-popularity=80?limit=30`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: "Bearer " + token,
+            },
           }
-        });
+        );
 
         const data = await result.json();
-        const num = (Math.floor(Math.random() * data.tracks.length));
+        const num = Math.floor(Math.random() * data.tracks.length);
         const targetData = data.tracks[num];
         const previewSongURL = targetData.preview_url;
         const previewImgURL = targetData.album.images[1].url;
@@ -110,10 +127,9 @@ const giveGenreToColor = async () => {
 
         detailDiv.style.display = "block";
 
-        detailDiv.innerHTML = '';
+        detailDiv.innerHTML = "";
 
-        const html =
-          `
+        const html = `
           <div class="musicPlayContainer" id="music_player">
             <div>
               <img class="albumCover" src="${previewImgURL}">\
@@ -137,7 +153,7 @@ const giveGenreToColor = async () => {
           </div>
         `;
 
-        detailDiv.innerHTML += html
+        detailDiv.innerHTML += html;
 
         const backgroundColor = {
           black: "rgba(0, 0, 0, 0.9)",
@@ -148,115 +164,129 @@ const giveGenreToColor = async () => {
           orange: "rgb(255, 115, 0, 0.5)",
           red: "rgba(183, 21, 21, 0.5)",
           blue: "rgba(36, 36, 160, 0.9)",
-          green: "rgb(0, 83, 8, 0.9)"
-        }
+          green: "rgb(0, 83, 8, 0.9)",
+        };
 
-        document.getElementById("colorPick_section").style.transition = "1s"
-        document.getElementById("colorPick_section").style.backgroundColor = eval(`backgroundColor.${colorId}`)
+        document.getElementById("colorPick_section").style.transition = "1s";
+        document.getElementById("colorPick_section").style.backgroundColor =
+          eval(`backgroundColor.${colorId}`);
 
-        gsap.to(detailDiv, {
-          duration: .5,
-          x: "0",
-          scale: .95,
-        }).then(gsap.to(detailDiv, {
-          duration: .5,
-          x: "0",
-          opacity: 1,
-          ease: "power4.in",
-        }))
+        gsap
+          .to(detailDiv, {
+            duration: 0.5,
+            x: "0",
+            scale: 0.95,
+          })
+          .then(
+            gsap.to(detailDiv, {
+              duration: 0.5,
+              x: "0",
+              opacity: 1,
+              ease: "power4.in",
+            })
+          );
 
-        let audio = new Audio(previewSongURL)
+        let audio = new Audio(previewSongURL);
 
-        const playBtn = document.getElementById("play_btn")
-        const pauseBtn = document.getElementById("pause_btn")
-        const forwardBtn = document.getElementById("forward_btn")
-        const backwardBtn = document.getElementById("backward_btn")
-        const likeSongBtn = document.getElementById("like_song")
+        const playBtn = document.getElementById("play_btn");
+        const pauseBtn = document.getElementById("pause_btn");
+        const forwardBtn = document.getElementById("forward_btn");
+        const backwardBtn = document.getElementById("backward_btn");
+        const likeSongBtn = document.getElementById("like_song");
 
-        const anotherColorBtn = document.getElementById("anotherColor_btn")
-        const shuffleBtn = document.getElementById("shuffle_btn")
+        const anotherColorBtn = document.getElementById("anotherColor_btn");
+        const shuffleBtn = document.getElementById("shuffle_btn");
 
         anotherColorBtn.addEventListener("click", () => {
-          audio.pause()
-          document.getElementById("colorPick_section").style.removeProperty("background-color");
+          audio.pause();
+          document
+            .getElementById("colorPick_section")
+            .style.removeProperty("background-color");
 
-          gsap.to(detailDiv, {
-            duration: .5,
-            scale: .95,
-          }).then(gsap.to(detailDiv, {
-            duration: .5,
-            x: "200%",
-            opacity: 0,
-            ease: "power4.in",
-          }))
+          gsap
+            .to(detailDiv, {
+              duration: 0.5,
+              scale: 0.95,
+            })
+            .then(
+              gsap.to(detailDiv, {
+                duration: 0.5,
+                x: "200%",
+                opacity: 0,
+                ease: "power4.in",
+              })
+            );
 
-          gsap.to(colorPickSection, {
-            duration: 1,
-            scale: .9,
-          }).then(gsap.to(colorPickSection, {
-            duration: 2,
-            opacity: 1,
-            ease: "power4.out",
-            x: "0",
-            delay: .3
-          }))
+          gsap
+            .to(colorPickSection, {
+              duration: 1,
+              scale: 0.9,
+            })
+            .then(
+              gsap.to(colorPickSection, {
+                duration: 2,
+                opacity: 1,
+                ease: "power4.out",
+                x: "0",
+                delay: 0.3,
+              })
+            );
 
           setTimeout(() => {
             detailDiv.style.display = "none";
-          }, 500)
-        })
+          }, 500);
+        });
 
         shuffleBtn.addEventListener("click", () => {
-          audio.pause()
-          getTrackByGenre()
-          return
-        })
-
+          audio.pause();
+          getTrackByGenre();
+          return;
+        });
 
         playBtn.addEventListener("click", () => {
           if (audio.src == "https://musicor.netlify.app/null") {
-            const alertWindow = document.getElementById("UI_comment")
+            const alertWindow = document.getElementById("UI_comment");
             alertWindow.innerHTML = `
             <p style="color:red">Sorry, this music does not provide a preview</p>
             <p style="color:red">Please select a different music</p>
-            `
-            return
+            `;
+            return;
           } else {
-            audio.volume = 1,
-              audio.play()
-            return
+            (audio.volume = 1), audio.play();
+            return;
           }
-        })
+        });
 
         pauseBtn.addEventListener("click", () => {
-          audio.pause()
-          return
-        })
+          audio.pause();
+          return;
+        });
 
         forwardBtn.addEventListener("click", () => {
-          audio.pause()
-          getTrackByGenre()
-          return
-        })
+          audio.pause();
+          getTrackByGenre();
+          return;
+        });
 
         backwardBtn.addEventListener("click", () => {
-          audio.pause()
-          getTrackByGenre()
-          return
-        })
+          audio.pause();
+          getTrackByGenre();
+          return;
+        });
 
         likeSongBtn.addEventListener("click", (e) => {
           if (e.target.style.filter == false) {
-            e.target.style.filter = "invert(15%) sepia(81%) saturate(7389%) hue-rotate(358deg) brightness(102%) contrast(116%)"
+            e.target.style.filter =
+              "invert(15%) sepia(81%) saturate(7389%) hue-rotate(358deg) brightness(102%) contrast(116%)";
           } else {
-            e.target.style.removeProperty("filter")
+            e.target.style.removeProperty("filter");
           }
-        })
-      }
+        });
+      };
       getTrackByGenre();
     } else {
       return;
     }
-  })
+  });
 };
 giveGenreToColor();
